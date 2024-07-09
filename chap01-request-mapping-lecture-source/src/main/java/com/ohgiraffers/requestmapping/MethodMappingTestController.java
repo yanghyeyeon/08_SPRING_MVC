960e3a97e9926b41fb5900aed6c55a47e765a8b9
yanghyeyeon <yhy4058@gmail.com>
2024-07-09 11:08:58 +0900

    request mapping

--- chap01-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestmapping/MethodMappingTestController.java
+++ chap01-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestmapping/MethodMappingTestController.java
@@ -1,2 +1,23 @@
-package com.ohgiraffers.requestmapping;public class MethodMappingTestController {
+package com.ohgiraffers.requestmapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+* DispatcherServlet은 웹 요청을 받는 즉시 @Controller가 달린 컨트롤러 클래스에
+* 처리를 위임한다. 그 과정은 컨트롤러 클래스의 핸들러 메서드에 선언된
+* 다양한 @RequestMapping 설정 내용에 따른다.
+* */
+@Controller
+public class MethodMappingTestController {
+
+    // 1. 메소드 방식 미지정
+    @RequestMapping("/menu/regist")
+    public String registMenu(Model model) {
+
+       model.addAttribute("message","신규 메뉴 등록용 핸들러 메소드 호출함...");
+
+        return "page/mappingResult";
+    }
 }
