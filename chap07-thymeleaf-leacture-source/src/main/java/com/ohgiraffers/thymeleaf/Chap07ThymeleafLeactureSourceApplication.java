0ee89e406972b14ec1866ad92e894a56cce56f92
yanghyeyeon <yhy4058@gmail.com>
2024-07-31 11:40:00 +0900

    thymeleaf

---
+++ chap07-thymeleaf-leacture-source/src/main/java/com/ohgiraffers/thymeleaf/Chap07ThymeleafLeactureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap07ThymeleafLeactureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap07ThymeleafLeactureSourceApplication.class, args);
+    }
+
+}
