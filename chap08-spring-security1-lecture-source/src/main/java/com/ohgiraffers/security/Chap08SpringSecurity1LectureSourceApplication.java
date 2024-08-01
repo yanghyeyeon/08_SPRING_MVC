f0351f47d3adcbf09d8bf6f0a0d60ec0cffa3c17
yanghyeyeon <yhy4058@gmail.com>
2024-08-01 14:28:04 +0900

    security

---
+++ chap08-spring-security1-lecture-source/src/main/java/com/ohgiraffers/security/Chap08SpringSecurity1LectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap08SpringSecurity1LectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap08SpringSecurity1LectureSourceApplication.class, args);
+    }
+
+}
---
+++ chap08-spring-security1-lecture-source/src/test/java/com/ohgiraffers/security/Chap08SpringSecurity1LectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap08SpringSecurity1LectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
