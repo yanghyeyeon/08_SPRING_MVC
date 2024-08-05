a89db6cee1b2af81a66c319b6c626e694a13ffc9
yanghyeyeon <yhy4058@gmail.com>
2024-08-05 09:59:01 +0900

    security2

---
+++ chap09-spring-security2-lecture-source/src/main/java/com/ohgiraffers/example/Chap09SpringSecurity2LectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap09SpringSecurity2LectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap09SpringSecurity2LectureSourceApplication.class, args);
+    }
+
+}
---
+++ chap09-spring-security2-lecture-source/src/test/java/com/ohgiraffers/example/Chap09SpringSecurity2LectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap09SpringSecurity2LectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
